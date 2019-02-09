091e6bb8593c9c2d385b368742b3b978bd313d7a
Chi Chi <malachijb28@gmail.com>
2019-02-09 10:10:29 -0500

    change if structure to switch structure and state based not button based

--- src/main/java/org/usfirst/frc3534/RobotBasic/systems/Elevator.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/systems/Elevator.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc3534.RobotBasic.systems;
 
 import org.usfirst.frc3534.RobotBasic.RobotMap;
-import org.usfirst.frc3534.RobotBasic.XboxPlusPOV.POV;
-
-import org.usfirst.frc3534.RobotBasic.Robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -19,33 +16,28 @@ public class Elevator extends SystemBase implements SystemInterface {
 
     private DigitalInput limitSwitch = RobotMap.limitSwitch;
 
-    private final int DPAD_DOWN;
-    private final int DPAD_UP;
-    private final int DPAD_RIGHT;
+    CylinderState cylinder1 = CylinderState.COLLAPSED;
+    CylinderState cylinder2 = CylinderState.COLLAPSED;
 
-    STATE cylinder1 = STATE.COLLAPSED;
-    STATE cylinder2 = STATE.COLLAPSED;
+    ElevatorState elevatorState = ElevatorState.Floor;
 
     public Elevator() {
-
-        //setting the DPAD buttons up so we dont have to type POV.Direction.getValue()
-        DPAD_DOWN = POV.South.getValue();
-        DPAD_UP = POV.North.getValue();
-        DPAD_RIGHT = POV.East.getValue();
 
     }
 
     @Override
     public void process() {
 
-        if(Robot.oi.getController2().getPOV() == DPAD_DOWN) {//DPAD DOWN, STAGE 1A
+        switch(elevatorState){
 
-            if (cylinder1 == STATE.EXTENDED) {//if the first cylinder is extended
+        case Stage1A:
+
+            if (cylinder1 == CylinderState.EXTENDED) {//if the first cylinder is extended
 
                 if (limitSwitch.get()) {//if the first cylinder is at the limit switch
 
                     limitSwitchMet = true;
-                    cylinder1 = STATE.HALFWAY;
+                    cylinder1 = CylinderState.HALFWAY;
 
                 }
                 if (!limitSwitchMet) {//if the limit switch is not met
@@ -60,12 +52,12 @@ public class Elevator extends SystemBase implements SystemInterface {
 
                 }
 
-            } else if (cylinder1 == STATE.COLLAPSED) {//if the first cylinder is collapsed
+            } else if (cylinder1 == CylinderState.COLLAPSED) {//if the first cylinder is collapsed
 
                 if (limitSwitch.get()) {//if the first cylinder is at the limit switch
 
                     limitSwitchMet = true;
-                    cylinder1 = STATE.HALFWAY;
+                    cylinder1 = CylinderState.HALFWAY;
 
                 }
                 if (!limitSwitchMet) {//if the limit switch is not met
@@ -83,71 +75,96 @@ public class Elevator extends SystemBase implements SystemInterface {
 
             } 
 
-        } else if (Robot.oi.getController2().getPOV() == DPAD_RIGHT) {//DPAD RIGHT, STAGE 1B
+            break;
+
+        case Stage1B:
 
             setCylinder1Extended();
             setCylinder2Collapsed();
-            if(cylinder1 == STATE.COLLAPSED){
+            if(cylinder1 == CylinderState.COLLAPSED){
 
                 if(limitSwitch.get()){
 
-                    cylinder1 = STATE.EXTENDED;
+                    cylinder1 = CylinderState.EXTENDED;
 
                 }
 
             }else{
 
-                cylinder1 = STATE.EXTENDED;
+                cylinder1 = CylinderState.EXTENDED;
 
             }
-            cylinder2 = STATE.COLLAPSED;
+            cylinder2 = CylinderState.COLLAPSED;
 
-        } else if(Robot.oi.getController2().getPOV() == DPAD_UP) {//DPAD UP, STAGE 2
+            break;
+
+        case Stage2:
 
             setCylinder1Extended();
             setCylinder2Extended();
-            if(cylinder1 == STATE.COLLAPSED){//if the first cylinder is collapsed 
+            if(cylinder1 == CylinderState.COLLAPSED){//if the first cylinder is collapsed 
 
                 if(limitSwitch.get()){//if the cylinder is at the limit switch
 
-                    cylinder1 = STATE.EXTENDED;
+                    cylinder1 = CylinderState.EXTENDED;
 
                 }
 
             }else{//if the first cylinder is not collapsed
 
-                cylinder1 = STATE.EXTENDED;
+                cylinder1 = CylinderState.EXTENDED;
 
             }
-            cylinder2 = STATE.EXTENDED;
+            cylinder2 = CylinderState.EXTENDED;
 
-        } else {//bottom stage, DPAD not pressed
+            break;
+
+        case Floor:
 
             setCylinder1Collapsed();
             setCylinder2Collapsed();
-            if(cylinder1 == STATE.EXTENDED){//if the first cylinder is extended
+            if(cylinder1 == CylinderState.EXTENDED){//if the first cylinder is extended
 
                 if(limitSwitch.get()){//if the cylinder is at the limit switch
 
-                    cylinder1 = STATE.COLLAPSED;
+                    cylinder1 = CylinderState.COLLAPSED;
 
                 }
 
             }else{//if the first cylinder is not extended
 
-                cylinder1 = STATE.COLLAPSED;
+                cylinder1 = CylinderState.COLLAPSED;
 
             }
+
+            break;
 
         }
 
     }
 
-    private enum STATE{
+    private enum CylinderState{
 
         EXTENDED,
         COLLAPSED,
         HALFWAY; //ONLY FOR CYLINDER 1
+
+    }
+
+    private enum ElevatorState{
+
+        Floor(1),
+        Stage1A(2),
+        Stage1B(3),
+        Stage2(4);
+
+        int value;
+
+        private ElevatorState(int value){
+
+            this.value = value;
+
+        }
 
     }
 
