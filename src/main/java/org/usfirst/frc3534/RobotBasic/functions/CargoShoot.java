04018852f46a11602b62949a7f355cff6c80fdf6
Chi Chi <malachijb28@gmail.com>
2019-02-15 17:56:45 -0500

    add cargo shoot function code

--- src/main/java/org/usfirst/frc3534/RobotBasic/functions/CargoShoot.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/functions/CargoShoot.java
@@ -1,30 +1,55 @@
 package org.usfirst.frc3534.RobotBasic.functions;
 
 import org.usfirst.frc3534.RobotBasic.Robot;
+import org.usfirst.frc3534.RobotBasic.systems.Shooter.ShooterState;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class CargoShoot extends FunctionBase implements FunctionInterface{
 
+    long originalTime = 0;
+
     public CargoShoot(){
 
         reset();
+        completed();
 
     }
 
     @Override
     public void process(){
 
+        if(!running && Robot.oi.getController2().getBumper(Hand.kRight)){
+
+            this.reset();
+
+        }
+
         switch(this.state) {
             case 0:
                 if(Robot.oi.getController2().getBumper(Hand.kRight)) {
                     this.started();
                     this.state = 10;
+                    originalTime = System.currentTimeMillis();
                 }
                 break;
     
             case 10:
+
+                Robot.shooter.setShooterState(ShooterState.SHOOT);
+
+                if(System.currentTimeMillis() - originalTime > .75 * 1000){
+
+                    this.state = 20;
+
+                }
                 
+                break;
+
+            case 20:
+
+                completed();
+
                 break;
     
             }
