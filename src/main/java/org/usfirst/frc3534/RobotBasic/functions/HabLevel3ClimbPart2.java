fbc9812390f2276503f5aadabe03335381d7db25
CalebEicherDev <calebeicher26@gmail.com>
2019-02-15 19:10:21 -0500

    added function to hab level 3 part 1 function process

--- src/main/java/org/usfirst/frc3534/RobotBasic/functions/HabLevel3ClimbPart2.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/functions/HabLevel3ClimbPart2.java
@@ -4,14 +4,31 @@ import org.usfirst.frc3534.RobotBasic.Robot;
 
 public class HabLevel3ClimbPart2 extends FunctionBase implements FunctionInterface{
 
+    long originalTime = 0;
+    boolean firstPartDone = false;
+
     public HabLevel3ClimbPart2(){
 
         reset();
+        completed();
 
     }
 
     @Override
     public void process(){
+
+        if(!Robot.oi.getController1().getXButton() && firstPartDone){
+
+            this.state = 40;
+            firstPartDone = false;
+
+        }
+
+        if(!running && Robot.oi.getController1().getXButton()){
+
+            this.reset();
+
+        }
 
         switch(this.state) {
             case 0:
