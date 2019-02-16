d99fdc367ba08746a815e250f3f3791b38798a4a
Chi Chi <malachijb28@gmail.com>
2019-02-15 19:09:20 -0500

    add xbutton reset function process

--- src/main/java/org/usfirst/frc3534/RobotBasic/functions/XButtonReset.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/functions/XButtonReset.java
@@ -1,17 +1,29 @@
 package org.usfirst.frc3534.RobotBasic.functions;
 
 import org.usfirst.frc3534.RobotBasic.Robot;
+import org.usfirst.frc3534.RobotBasic.systems.Elevator.ElevatorState;
+import org.usfirst.frc3534.RobotBasic.systems.Intake.ArmExtendState;
+import org.usfirst.frc3534.RobotBasic.systems.Intake.ArmLiftState;
 
 public class XButtonReset extends FunctionBase implements FunctionInterface {
+
+    long originalTime = 0;
 
     public XButtonReset() {
 
         this.reset();
+        this.completed();
 
     }
 
     @Override
     public void process() {
+
+        if(!running && Robot.oi.getController2().getBackButton()){
+
+            this.reset();
+
+        }
         
         switch(this.state) {
         case 0:
@@ -19,10 +31,45 @@ public class XButtonReset extends FunctionBase implements FunctionInterface {
             if(Robot.oi.getController2().getBackButton()) {
                 this.started();
                 this.state = 10;
+                originalTime = System.currentTimeMillis();
             }
             break;
 
         case 10:
+
+            Robot.intake.setArmLiftState(ArmLiftState.MID);
+
+            if(System.currentTimeMillis() - originalTime > .75 * 1000){
+
+                this.state = 20;
+
+            }
+            
+            break;
+
+        case 20:
+
+            Robot.intake.setArmExtendState(ArmExtendState.COLLAPSED);
+           
+            if(Robot.intake.isArmAft()){
+
+                this.state = 30;
+
+            }
+
+            break;
+
+        case 30:
+
+            Robot.elevator.setElevatorState(ElevatorState.Floor);
+
+            this.state = 40;
+
+            break;
+
+        case 40:
+
+            completed();
             
             break;
 
