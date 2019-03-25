135d3c3353beb5d66e4814022adb5711a060f2c4
Chi Chi <malachijb28@gmail.com>
2019-03-25 19:13:04 -0400

    clean up and organize robotmap

--- src/main/java/org/usfirst/frc3534/RobotBasic/RobotMap.java
+++ src/main/java/org/usfirst/frc3534/RobotBasic/RobotMap.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 /**
@@ -22,35 +21,31 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
  */
 public class RobotMap {
 
-	public static WPI_TalonSRX frontRightMotor;
-	private static WPI_TalonSRX backRightMotor;
-	public static WPI_TalonSRX frontLeftMotor;
-	private static WPI_TalonSRX backLeftMotor;
+	public static WPI_TalonSRX frontLeftMotor;		//1
+	public static WPI_TalonSRX centerLeftMotor;		//2
+	private static WPI_TalonSRX backLeftMotor;		//3
+	public static WPI_TalonSRX cargoRoller;			//4
+	//spare talon srx								//5
+	public static WPI_TalonSRX cargoShooterMaster;	//6
+	public static WPI_TalonSRX cargoShooterSlave;	//7
+	private static WPI_TalonSRX backRightMotor;		//8
+	public static WPI_TalonSRX centerRightMotor;	//9
+	public static WPI_TalonSRX frontRightMotor;		//10
 
 	public static SpeedControllerGroup rightSideMotors;
 	public static SpeedControllerGroup leftSideMotors;
+	public static SpeedControllerGroup cargoShooter;
+
+	public static DoubleSolenoid elevatorCylinderOne;		//first value -> PCM A, CHANEL 0, 1
+	public static DoubleSolenoid elevatorCylinderTwo;		//second value -> PCM A, CHANNEL 2, 3
+	public static DoubleSolenoid panelCylinders;			//third value -> PCM A, CHANNEL 4, 5
+	public static DoubleSolenoid intakeForwardAftCylinders;	//sixth value -> PCM B, CHANNEL 2, 3
+	public static DoubleSolenoid panelIntakeCylinders;		//eighth vlaue -> PCM B, CHANNEL 6, 7
 
 	public static AHRS navx;
 
-	public static DoubleSolenoid elevatorCylinderOne;
-	public static DoubleSolenoid elevatorCylinderTwo;
-
 	public static DigitalInput limitSwitch;
-
-	public static DoubleSolenoid intakeForwardAftCylinders;
-
 	public static DigitalInput rightArmSensor;
-
-	public static WPI_TalonSRX cargoShooterMaster;
-	public static WPI_TalonSRX cargoShooterSlave;
-
-	public static WPI_TalonSRX cargoRoller;
-
-	public static SpeedControllerGroup cargoShooter;
-
-	public static DoubleSolenoid panelCylinders;
-
-	public static DoubleSolenoid panelIntakeCylinders;
 
 	public static Compressor compressor;
 
@@ -68,49 +63,55 @@ public class RobotMap {
 
 	public static void init() {
 
-		frontRightMotor = new WPI_TalonSRX(10);
-		frontRightMotor.set(ControlMode.PercentOutput, 0);
-		frontRightMotor.setNeutralMode(NeutralMode.Brake);
-		frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
-
-		backRightMotor = new WPI_TalonSRX((9));
-		backRightMotor.setNeutralMode(NeutralMode.Brake);
-		backRightMotor.set(ControlMode.PercentOutput, 0);
-
 		frontLeftMotor = new WPI_TalonSRX(1);
 		frontLeftMotor.setNeutralMode(NeutralMode.Brake);
 		frontLeftMotor.set(ControlMode.PercentOutput, 0);
 		frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 
-		backLeftMotor = new WPI_TalonSRX(2);
+		centerLeftMotor = new WPI_TalonSRX(2);
+		centerLeftMotor.setNeutralMode(NeutralMode.Brake);
+		centerLeftMotor.set(ControlMode.PercentOutput, 0);
+
+		backLeftMotor = new WPI_TalonSRX(3);
 		backLeftMotor.setNeutralMode(NeutralMode.Brake);
 		backLeftMotor.set(ControlMode.PercentOutput, 0);
 
-		rightSideMotors = new SpeedControllerGroup(frontRightMotor, backRightMotor);
-		leftSideMotors = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
+		cargoRoller = new WPI_TalonSRX(4);
 
-		navx = new AHRS(SPI.Port.kMXP);
-
-		elevatorCylinderOne = new DoubleSolenoid(1, 0, 1);
-		elevatorCylinderTwo = new DoubleSolenoid(1, 2, 3);
-
-		limitSwitch = new DigitalInput(0);
-
-		intakeForwardAftCylinders = new DoubleSolenoid(2, 2, 3);
-
-		rightArmSensor = new DigitalInput(1);
+		//spare talon srx
 
 		cargoShooterMaster = new WPI_TalonSRX(6);
+
 		cargoShooterSlave = new WPI_TalonSRX(7);
 		cargoShooterSlave.setInverted(true);
 
+		backRightMotor = new WPI_TalonSRX((9));
+		backRightMotor.setNeutralMode(NeutralMode.Brake);
+		backRightMotor.set(ControlMode.PercentOutput, 0);
+
+		centerRightMotor = new WPI_TalonSRX((9));
+		centerRightMotor.setNeutralMode(NeutralMode.Brake);
+		centerRightMotor.set(ControlMode.PercentOutput, 0);
+
+		frontRightMotor = new WPI_TalonSRX(10);
+		frontRightMotor.set(ControlMode.PercentOutput, 0);
+		frontRightMotor.setNeutralMode(NeutralMode.Brake);
+		frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+
+		rightSideMotors = new SpeedControllerGroup(frontRightMotor, backRightMotor);
+		leftSideMotors = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
 		cargoShooter = new SpeedControllerGroup(cargoShooterMaster, cargoShooterSlave);
 
-		cargoRoller = new WPI_TalonSRX(4);
-
+		elevatorCylinderOne = new DoubleSolenoid(1, 0, 1);
+		elevatorCylinderTwo = new DoubleSolenoid(1, 2, 3);
 		panelCylinders = new DoubleSolenoid(1, 4, 5);
-
+		intakeForwardAftCylinders = new DoubleSolenoid(2, 2, 3);
 		panelIntakeCylinders = new DoubleSolenoid(2, 6, 7);
+
+		navx = new AHRS(SPI.Port.kMXP);
+
+		limitSwitch = new DigitalInput(0);
+		rightArmSensor = new DigitalInput(1);
 
 		compressor = new Compressor(1);
         compressor.setClosedLoopControl(true);
